181cff5ac00c023b174d93535bf4557e59968fbb
Alexander Klassanov <klassanov@gmail.com>
2020-02-05 19:19:44 +0200

    Removed unused folder and project

--- TransactionsCalculator.WebClients/FrankfurterApiClient.cs
+++
@@ -1,8 +0,0 @@
-﻿using TransactionsCalculator.Interfaces.WebClients;
-
-namespace TransactionsCalculator.WebClients
-{
-    public class FrankfurterApiClient : IExchangeRateApiClient
-    {
-    }
-}
