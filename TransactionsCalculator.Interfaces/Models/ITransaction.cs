78c24753d19ab3e882ccc47d4393413bf18a8616
Alexander Klassanov <klassanov@gmail.com>
2020-04-25 22:13:46 +0300

    Column names put as summary

--- TransactionsCalculator.Interfaces/Models/ITransaction.cs
+++ TransactionsCalculator.Interfaces/Models/ITransaction.cs
@@ -4,26 +4,59 @@ namespace TransactionsCalculator.Interfaces.Models
 {
     public interface ITransaction
     {
+        /// <summary>
+        /// BP: SALE_ARRIVAL_COUNTRY
+        /// </summary>
         string SaleArrivalCountry { get; set; }
 
+        /// <summary>
+        /// BO: SALE_DEPART_COUNTRY
+        /// </summary>
         string SaleDepartureCountry { get; set; }
 
+        /// <summary>
+        /// BW: TRANSACTION_SELLER_VAT_NUMBER_COUNTRY
+        /// </summary>
         string TransactionSellerVATNumberCountry { get; set; }
 
+        /// <summary>
+        /// BA: TRANSACTION_CURRENCY_CODE
+        /// </summary>
         string TransactionCurrencyCode { get; set; }
 
+        /// <summary>
+        /// BY: BUYER_VAT_NUMBER_COUNTRY
+        /// </summary>
         string BuyerVATNumberCountry { get; set; }
 
+        /// <summary>
+        /// AZ: TOTAL_ACTIVITY_VALUE_AMT_VAT_INCL
+        /// </summary>
         decimal? TotalActivityVATIncludedAmount { get; set; }
 
+        /// <summary>
+        /// AP: TOTAL_ACTIVITY_VALUE_VAT_AMT
+        /// </summary>
         decimal? TotalActivityVATAmount { get; set; }
 
+        /// <summary>
+        /// CB: TAXABLE_JURISDICTION
+        /// </summary>
         public string TaxableJurisdiction { get; set; }
 
+        /// <summary>
+        /// K: TRANSACTION_COMPLETE_DATE
+        /// </summary>
         public DateTime? TransactionCompleteDate { get; set; }
 
+        /// <summary>
+        /// BX: TRANSACTION_SELLER_VAT_NUMBER
+        /// </summary>
         public string TransactionSellerVATNumber { get; set; }
 
+        /// <summary>
+        /// BZ: BUYER_VAT_NUMBER
+        /// </summary>
         public string BuyerVATNumber { get; set; }
     }
 }
